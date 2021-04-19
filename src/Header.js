739693d4d07e054ef84185e0588b3788adb956eb
gusbikos <gusbikos@gmail.com>
2021-04-19 14:29:17 -0400

    Start server

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 // This will contain the buttons for Login, Logout, Signup
 
 function Header(){
-
+    return (
+        <h1> Header </h1>
+    )
 }
 
 export default Header;
